f0b622e2a606e766bc8ed671a11b09a90dbde63e
cherep121 <antonchistyuhin@gmail.com>
2026-09-10 16:07:17 +0700

    Completing tasks 4 and 5.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -51,6 +51,68 @@ fun task3() {
     }
 }
 
+// 4. Первые n простых чисел
+fun isPrime(number: Int): Boolean {
+    if (number < 2) return false
+    for (i in 2 until number) {
+        if (number % i == 0) {
+            return false
+        }
+    }
+    return true
+}
+
+fun task4() {
+    print("Сколько простых чисел вывести? ")
+    val n = readln().toInt()
+
+    var count = 0
+    var number = 2
+
+    while (count < n) {
+        if (isPrime(number)) {
+            count++
+            println("$count-е простое число: $number")
+        }
+        number++
+    }
+}
+
+// 5. Элементы массива, которые больше обоих соседей
+fun task5() {
+    val array = intArrayOf(1, 5, 2, 8, 3, 7, 4)
+
+    println("Массив: ${array.joinToString()}")
+
+    print("for: ")
+    for (i in 1 until array.lastIndex) {
+        if (array[i] > array[i - 1] && array[i] > array[i + 1]) {
+            print("${array[i]} ")
+        }
+    }
+    println()
+
+    print("while: ")
+    var i = 1
+    while (i < array.lastIndex) {
+        if (array[i] > array[i - 1] && array[i] > array[i + 1]) {
+            print("${array[i]} ")
+        }
+        i++
+    }
+    println()
+
+    print("forEach: ")
+    array.withIndex().forEach { (index, value) ->
+        if (index > 0 && index < array.lastIndex &&
+            value > array[index - 1] && value > array[index + 1]
+        ) {
+            print("$value ")
+        }
+    }
+    println()
+}
+
 fun main() {
     println("Лабораторная работа №0: Основы Kotlin")
     println("Выберите задание 1-10:")
